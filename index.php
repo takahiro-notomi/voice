f70da03d35ee755c90fabf7ab1ce49d601b9bb43
takahiro <130128593+takahiro-notomi@users.noreply.github.com>
2024-11-06 15:19:09 +0900

    Update index.php

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/num_pad.css">
+    <link rel="stylesheet" href="./num_pad.css">
     <title>iU voice Navi</title>
 </head>
 <body>
@@ -62,10 +62,8 @@
     </main>
 <!-- ===↓audio↓=============================================== -->
 
-<audio src="<?php ?>"></audio>
-
 <!-- ===↓script↓======================================= -->
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
-    <script src="./js/num_pad.js"></script>
+    <script src="./num_pad.js"></script>
 </body>
 </html>
